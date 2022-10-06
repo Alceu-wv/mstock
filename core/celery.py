387b17ca974f7a5746eb640ef0360419d763b834
Alceu-wv <alceu.wv@gmail.com>
2022-10-06 22:56:46 +0000

    broken Celery

---
+++ core/celery.py
@@ -0,0 +1,42 @@
+import os
+
+from asbool import asbool
+from celery import Celery
+from celery.schedules import crontab
+from celery.signals import worker_process_init
+
+# set the default Django settings module for the "celery" program.
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings")
+
+
+@worker_process_init.connect(weak=False)
+def init_celery_tracing(*args, **kwargs):
+    if not asbool(os.environ.get("OTEL_EXPORTER_ACTIVE")):
+        return ""
+
+
+app = Celery("mstock_app")
+
+# Using a string here means the worker doesn"t have to serialize
+# the configuration object to child processes.
+# - namespace="CELERY" means all celery-related configuration keys
+#   should have a `CELERY_` prefix.
+app.config_from_object("django.conf:settings", namespace="CELERY")
+
+# Load task modules from all registered Django app configs.
+app.autodiscover_tasks()
+
+app.conf.timezone = "UTC"
+
+app.conf.beat_schedule = {
+    "health_check": {
+        "task": "worker_health_check",
+        "schedule": crontab(minute="*/1"),
+    },
+}
+
+CELERY_BROKER_URL = "redis://redis:5672"
+CELERY_RESULT_BACKEND = "redis://redis:5672"
+
+# CELERY_BROKER_URL = "redis://redis:6379"
+# CELERY_RESULT_BACKEND = "redis://redis:6379"
--- mstock_app/apps.py
+++ mstock_app/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class SideProjectConfig(AppConfig):
+class MstockAppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'side_project'
+    name = 'mstock_app'
